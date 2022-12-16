74ae3829107fae3c206438fcc5b50a77a271b1f0
RodrigoMarques <rodrigomarques328@hotmail.com>
2022-12-16 18:00:37 -0300

    EXERCICIO

--- exercicio.js
+++ exercicio.js
@@ -53,3 +53,6 @@ resultadoFinal()
 
 
 // -----------------------------------------------------------------
+// Algumas observações:
+// O ".map" permite que você retorne um array a partir de um array existente;
+//  Verificar no vídeo: https://www.youtube.com/watch?v=A8ATE1d_2xM
